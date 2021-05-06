3b5a4797b645ef5acd4e0b3f710966fb29f11364
Adsigma <hong@cloudjetkpi.com>
2021-05-06 15:10:03 +0700

    whales

--- src/components/xbc/HeroSplit.vue
+++ src/components/xbc/HeroSplit.vue
@@ -28,7 +28,7 @@
                           XBC · XBN Community · Hold XBC to Earn BNB
                         </h1>
                         <p class="mt-0 mb-24 reveal-from-left" data-reveal-delay="200">
-                            <span class="subtitle">XBC is forked from Moonrat.Finance, it is a new brilliant & innovative protocol. XBC is fair launch, driven by community run on #BSC</span>
+                            <span class="subtitle">XBC is a new brilliant & innovative protocol which give Holders passive income: just hold XBC and earn BNB, no need to sell token to pay for yourselves. XBC is fair launch, by community, for community. Unlike Safemoon, XBC has anti-whales features(<a target="_blank" href="https://docs.xbc.xbn.finance/innovation/anti-whales">read more</a>)</span>
                             <br/>
                             <br/>
                           XBC has been fixed all security issues on Smart Contract by security experts and ready to meet you at the moon
